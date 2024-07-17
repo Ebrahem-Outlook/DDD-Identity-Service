09f8a2085eed86537dea87f8669e8c5020e4e5a0
Ebrahem-Outlook <EbrahemElbetar1@outlook.com>
2024-07-17 18:57:36 +0300

    V_9

---
+++ Identity-Domain/Core/BaseType/Result/ResultT.cs
@@ -0,0 +1,12 @@
+﻿namespace Identity_Domain.Core.BaseType.Result;
+
+public class Result<TValue> : Result where TValue : class
+{
+    protected internal Result(TValue value, bool isSuccess, Error error) 
+        : base(isSuccess, error)
+    {
+
+    }
+
+
+}
